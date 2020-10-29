507ca659943f46229a1616bef6e52bc47f6c5d31
Niklas Little <niklas@tomlittle.org>
2020-10-29 18:32:35 +0100

    implementing SEPA with the new stripe

--- routes/stripeNew.js
+++ routes/stripeNew.js
@@ -10,6 +10,8 @@ const stripe = require("stripe")(
 	"sk_test_51HJwTAKuLkk2F1U97GRH3SlNqdgvapCu4yWL0LmMmPHpQ6qGCKtYC7d7QhNT3ryjxOjoNH8nONvpGC6gWHxF4e3z004M2cPo3l"
 );
 
+const { v4: uuidv4 } = require("uuid");
+
 router.use(express.static("."));
 
 const calculateOrderAmount = (price) => {
@@ -30,6 +32,25 @@ router.post("/create-payment-intent", async (req, res) => {
 	const paymentIntent = await stripe.paymentIntents.create({
 		amount: calculateOrderAmount(price),
 		currency: "eur",
+	});
+
+	res.send({
+		clientSecret: paymentIntent.client_secret,
+	});
+});
+
+router.post("/create-iban-payment-intent", async (req, res) => {
+	const { price } = req.body;
+
+	// Create a PaymentIntent with the order amount and currency
+
+	const paymentIntent = await stripe.paymentIntents.create({
+		amount: calculateOrderAmount(price),
+		currency: "eur",
+		// setup_future_usage: 'off_session',
+		payment_method_types: ['sepa_debit'],
+		// Verify your integration in this guide by including this parameter
+		metadata: {integration_check: 'sepa_debit_accept_a_payment'},
 	});
 
 	res.send({
